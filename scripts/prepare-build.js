0f760401fe94d15fdd1ae02d7965938e18acd878
Guido Doumen <incyclist@doumen-laumen.de>
2024-10-26 13:37:24 +0200

    create linux release 0.9.8

--- scripts/prepare-build.js
+++ scripts/prepare-build.js
@@ -6,23 +6,59 @@ const package_json = require('../package.json')
 const os = require('os');
 const { ZipData } = require('../src/modules/zip')
 const { checkDir } = require('../src/utils')
-const { version,name,build} = package_json
+const { version,name} = package_json
 const lnk = require('lnk')
+const readline = require('node:readline');
 
+
+let config = process.argv[2]??'../package.json'
+
+if (config.startsWith('./')) config='.'+config
+
+
+const {build} = require(config)??{}
+
+console.log( 'config:', build)
 console.log('Platform:', os.platform(),build[os.platform()])
-const buildResources = build[os.platform()]?.directories?.buildResources || "./res"
+const buildResources = build?.directories?.buildResources || "./res"
 const buildDir = path.join(baseDir,`release/${os.platform()}`)
-
 const appBuildDir = path.join(buildDir,buildResources)
 
 
 function cleanupDirectory(dir) {
+
+    console.log('cleanup build dir')
+    const unlink = (l) => { try {fs.unlinkSync(l)} catch (err){console.log(err)}}
+
+    // clean build directory
+    unlink(path.join(buildDir,'./node_modules'))
+    unlink(path.join(buildDir,'./bin'))
+    unlink(path.join(buildDir,'./src'))
+    unlink(path.join(buildDir,'./scripts'))
+    
     try {
         try {fs.rmSync(dir,{recursive:true});} catch{}
         checkDir(dir)    
     } catch (err) {
         console.log(err)
     }
+
+    console.log('cleanup build dir done')
+
+}
+
+function getReactVersionFromUser() {
+    return new Promise (resolve =>{
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+          });
+          rl.question(`UI Version?`, version => {
+            rl.close();
+            resolve(version)
+          });
+    
+    })
 }
 
 async function copyFromServer(url) {
@@ -30,9 +66,15 @@ async function copyFromServer(url) {
     axios.defaults.headers.common = { "X-uuid":"update-react","X-arch":os.arch(), "X-platform":os.platform(), "user-agent": `update-react/${version} (${os.platform()};${os.arch()};${os.release()})`};
 
     console.log('checking version ...')
-    let response = await axios.get(`${url}/api/v1/apps/${name}/${version}?uuid=update-react`) || {}
-    const {reactVersion} = response.data||{};
-    if (!reactVersion)
+    let response = await axios.get(`${url}/api/v1/apps/${name}/${version}?uuid=update-react`)    
+    let {reactVersion} = response?.data
+
+    if (!reactVersion?.length) {
+        reactVersion = await getReactVersionFromUser();
+    }
+
+
+    if (!reactVersion) 
         throw new Error('could not identify react version')
 
 
@@ -77,6 +119,7 @@ checkDir(buildResources)
 
 fs.cpSync(path.join(baseDir,'./res'),appBuildDir, {recursive: true})
 
+
 if (os.platform()==='win32') {
     lnk.sync(path.join(baseDir,'./node_modules'),buildDir)
     lnk.sync(path.join(baseDir,'./bin'),buildDir)
@@ -92,16 +135,21 @@ else {
     
 }
 
-['win32','darwin','linux'].forEach( p=> {
-    if (p!==os.platform())
-        delete package_json.build[p]
-})
-
-package_json.build.directories = package_json.build[os.platform()]?.directories
-if (package_json.build.directories){
-    if (package_json.build.directories.output)
-        package_json.build.directories.output = path.join(__dirname, package_json.build.directories.output  )
-    delete package_json .build[os.platform()].directories
+if (!package_json.build) {
+    package_json.build = build
+}
+else {
+    ['win32','darwin','linux'].forEach( p=> {
+        if (p!==os.platform())
+            delete package_json.build[p]
+    })   
+    
+    package_json.build.directories = package_json.build[os.platform()]?.directories
+    if (package_json.build.directories){
+        if (package_json.build.directories.output)
+            package_json.build.directories.output = path.join(__dirname, package_json.build.directories.output  )
+        delete package_json.build[os.platform()].directories
+    }
 }
 fs.writeFileSync( path.join(buildDir,'./package.json'), JSON.stringify(package_json,null,2 ))
 

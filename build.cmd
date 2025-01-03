aad11d59206dd517fffc5901d03871af79b640fe
Guido Doumen <guido@incyclist.com>
2025-01-03 18:12:52 +0100

    remove deprecated files

--- build.cmd
+++
@@ -1,9 +0,0 @@
-rmdir /S /Q release-win
-rmdir /S /Q release
-rmdir /S /Q node_modules
-npm install
-electron-rebuild
-npm run package-win64
-npm run installer-win64
-#electron-packager ./ --platform=win32 --arch=x64 --icon=app/favicon.ico --out=release/ergoapp
-#node build.js
